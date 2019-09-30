ad3eff36c2bc44cbee386fed3d245decd1f1b4fe
molliemarie <mollie.marie@gmail.com>
2019-09-30 16:17:42 -0700

    replaced static currently reading code with code that loops through currently reading books and displays

--- src/App.js
+++ src/App.js
@@ -78,6 +78,51 @@ class BooksApp extends Component {
             </div>
             <div className="list-books-content">
               <div>
+                //
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {currentlyReading.map(book => (
+                        <li key={book.id}>
+                          <div className="book">
+                            <div className="book-top">
+                              <div
+                                className="book-cover"
+                                className="book-cover"
+                                style={{
+                                  width: 128,
+                                  height: 193,
+                                  backgroundImage: `url(${
+                                    book.imageLinks.smallThumbnail
+                                  })`
+                                }}
+                              />
+                              <div className="book-shelf-changer">
+                                <select>
+                                  <option value="move" disabled>
+                                    Move to...
+                                  </option>
+                                  <option value="currentlyReading">
+                                    Currently Reading
+                                  </option>
+                                  <option value="wantToRead">
+                                    Want to Read
+                                  </option>
+                                  <option value="read">Read</option>
+                                  <option value="none">None</option>
+                                </select>
+                              </div>
+                            </div>
+                            <div className="book-title">{book.title}</div>
+                            <div className="book-authors">{book.authors}</div>
+                          </div>
+                        </li>
+                      ))}
+                    </ol>
+                  </div>
+                </div>
+                //
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
