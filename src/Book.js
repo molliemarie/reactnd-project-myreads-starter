26a3c960cdf5df8bff95c04be75880365b08a86c
molliemarie <mollie.marie@gmail.com>
2019-09-30 17:26:24 -0700

    moved book code into Book component

--- src/Book.js
+++ src/Book.js
@@ -1,7 +1,39 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-class Books extends Component {
+class Book extends Component {
   // TODO: PropTypes
-  // TODO: Everything else!
+  render() {
+    const { book } = this.props;
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+            }}
+          />
+          <div className="book-shelf-changer">
+            <select>
+              <option value="move" disabled>
+                Move to...
+              </option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors}</div>
+      </div>
+    );
+  }
 }
+
+export default Book;
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import Book from "./Book";
 
 class ListBooks extends Component {
   // TODO: PropTypes
@@ -10,32 +11,7 @@ class ListBooks extends Component {
       <ol className="books-grid">
         {filteredBooks.map(book => (
           <li key={book.id}>
-            <div className="book">
-              <div className="book-top">
-                <div
-                  className="book-cover"
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 193,
-                    backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-                  }}
-                />
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="move" disabled>
-                      Move to...
-                    </option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">{book.authors}</div>
-            </div>
+            <Book book={book} />
           </li>
         ))}
       </ol>
