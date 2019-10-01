ef83c608e08106c4da2cd83edeb595887d7b4303
molliemarie <mollie.marie@gmail.com>
2019-09-30 17:20:05 -0700

    moved code listing books into the ListBooks component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -4,4 +4,43 @@ import PropTypes from "prop-types";
 class ListBooks extends Component {
   // TODO: PropTypes
   // TODO: Everything else!
+  render() {
+    const { filteredBooks, title } = this.props;
+    return (
+      <ol className="books-grid">
+        {filteredBooks.map(book => (
+          <li key={book.id}>
+            <div className="book">
+              <div className="book-top">
+                <div
+                  className="book-cover"
+                  className="book-cover"
+                  style={{
+                    width: 128,
+                    height: 193,
+                    backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+                  }}
+                />
+                <div className="book-shelf-changer">
+                  <select>
+                    <option value="move" disabled>
+                      Move to...
+                    </option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                  </select>
+                </div>
+              </div>
+              <div className="book-title">{book.title}</div>
+              <div className="book-authors">{book.authors}</div>
+            </div>
+          </li>
+        ))}
+      </ol>
+    );
+  }
 }
+
+export default ListBooks;
--- src/Shelf.js
+++ src/Shelf.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import ListBooks from "./ListBooks";
 
 class Shelf extends Component {
   // TODO: PropTypes
@@ -9,42 +10,7 @@ class Shelf extends Component {
       <div className="bookshelf">
         <h2 className="bookshelf-title">{title}</h2>
         <div className="bookshelf-books">
-          <ol className="books-grid">
-            {filteredBooks.map(book => (
-              <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div
-                      className="book-cover"
-                      className="book-cover"
-                      style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage: `url(${
-                          book.imageLinks.smallThumbnail
-                        })`
-                      }}
-                    />
-                    <div className="book-shelf-changer">
-                      <select>
-                        <option value="move" disabled>
-                          Move to...
-                        </option>
-                        <option value="currentlyReading">
-                          Currently Reading
-                        </option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.title}</div>
-                  <div className="book-authors">{book.authors}</div>
-                </div>
-              </li>
-            ))}
-          </ol>
+          <ListBooks filteredBooks={filteredBooks} title={title} />
         </div>
       </div>
     );
